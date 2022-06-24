761a67e56f244bd3f93c64770fd70598c6ae040b
BDSEERDEN <bodoekeleseerden@gmail.com>
2022-06-24 02:48:24 +0200

    Created route to get specific space with stories included

--- routers/space.js
+++ routers/space.js
@@ -1,5 +1,6 @@
 const { Router } = require("express");
 const Space = require("../models").space;
+const Story = require("../models").story;
 const spacesRouter = new Router();
 
 spacesRouter.get("/", async (request, response, next) => {
@@ -9,6 +10,26 @@ spacesRouter.get("/", async (request, response, next) => {
   } catch (e) {
     next(e.message);
   }
+});
+
+spacesRouter.get("/:id", async (req, res) => {
+  const { id } = req.params;
+
+  console.log(id);
+  if (isNaN(parseInt(id))) {
+    return res.status(400).send({ message: "Space id is not a number" });
+  }
+
+  const space = await Space.findByPk(id, {
+    include: [Story],
+    order: [[Story, "createdAt", "DESC"]],
+  });
+
+  if (space === null) {
+    return res.status(404).send({ message: "Space not found" });
+  }
+
+  res.status(200).send({ message: "ok", space });
 });
 
 module.exports = spacesRouter;
